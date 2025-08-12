65bb4ed393190026787eda5f0420dc09db26189c
dustinstanek <62271277+dustinstanek@users.noreply.github.com>
2025-08-12 04:48:59 -0700

    chore: add root package manifest

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+export default [
+  {
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+      globals: {
+        process: "readonly",
+        console: "readonly",
+      },
+    },
+    rules: {},
+  },
+];
